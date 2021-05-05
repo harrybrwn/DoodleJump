07fb77aff196abd325bbaf1bee92c24184291ac0
ssareddy <63922647+ssareddy@users.noreply.github.com>
2021-05-04 19:59:22 -0700

    Add files via upload

---
+++ PlatformCtrl.h
@@ -0,0 +1,140 @@
+#pragma once
+
+#include <SFML/Graphics/RectangleShape.hpp>
+#include <random>
+#include <ctime>
+
+/**
+ * Platfor controller for managing and generating platforms
+ */
+class PlatformCtrl {
+    static const int PlatformWidth = 120;
+
+    int n;
+    struct { int w, h; } screen;
+    sf::RectangleShape* platforms;
+    bool hit_check = false;
+    std::default_random_engine rng_engine;
+    struct {
+        std::uniform_int_distribution<int> x;
+        std::uniform_int_distribution<int> y;
+    } rng;
+
+public:
+    PlatformCtrl(sf::RectangleShape platforms[], int n)
+      : platforms(platforms), n(n), rng_engine(time(0))
+    {
+        sf::Vector2f size(PlatformWidth, 20);
+        for (int i = 0; i < n; i++)
+        {
+            platforms[i].setSize(size);
+            platforms[i].setPosition(0, 0);
+            platforms[i].setFillColor(sf::Color::Black);
+        }
+    }
+
+    void set_bounds(int w, int h)
+    {
+        // TODO Make this function an option as a contructor
+        screen.w = w;
+        screen.h = h;
+        rng.x = std::uniform_int_distribution<int>(PlatformWidth/2, w-PlatformWidth);
+        rng.y = std::uniform_int_distribution<int>(0, w / n);
+    }
+
+    void randomize(int w, int h)
+    {
+        // TODO This overload is dumb and i hate it, probably get rid of it.
+        set_bounds(w, h);
+        randomize();
+    }
+
+    void randomize()
+    {
+        int x, y;
+        // Divide the screen vertically into "n" regions
+        // and generate a random height inside each region
+        // so the platform heigh feels random but really isn't
+        // that random because players still need to be able
+        // to get to the top.
+        const int section_width = screen.h / n;
+
+        for (int i = 0; i < n; i++)
+        {
+            x = rng.x(rng_engine);
+            y = rng.y(rng_engine);
+            platforms[i].setPosition(x, y+(i*section_width));
+        }
+    }
+
+    bool hit_platform(sf::FloatRect box)
+    {
+        sf::FloatRect bounds;
+        for (int i = 0; i < n; i++)
+        {
+            bounds = platforms[i].getGlobalBounds();
+            if (bounds.intersects(box))
+            {
+                hit_check = true;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    bool hit_platform(float x, float y)
+    {
+        //sf::FloatRect bounds;
+        for (int i = 0; i < n; i++)
+        {
+            sf::Vector2f bounds = platforms[i].getPosition();
+            if(y < bounds.y && y > bounds.y - 7)
+            {
+                if (x > bounds.x && x < bounds.x + PlatformWidth)
+                {
+                    hit_check = true;
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    bool landed_on()
+    {
+        if(hit_check)
+        {
+            return true;
+        }
+
+        return false;
+
+    }
+
+    int width_getter()
+    {
+        return PlatformWidth;
+    }
+
+    void shift_up(int y);
+};
+
+void PlatformCtrl::shift_up(int y)
+{
+    sf::Vector2f pos;
+    for (int i = 0; i < n; i++)
+    {
+        pos = platforms[i].getPosition();
+        if (pos.y > screen.h)
+        {
+            // generate a new position for the platform
+            // make sure its off-screen
+            platforms[i].setPosition(rng.x(rng_engine), -50);
+        }
+        else
+        {
+            pos.y += y;
+            platforms[i].setPosition(pos);
+        }
+    }
+}
--- main.cpp
+++ main.cpp
@@ -1,11 +1,159 @@
-#include "mainwindow.h"
+#include <SFML/Graphics.hpp>
 
-#include <QApplication>
+#include <random>
+#include <string>
+#include <iostream>
+#include <stdlib.h>
 
-int main(int argc, char *argv[])
+using namespace std;
+
+#include "PlatformCtrl.h"
+
+using namespace std;
+
+// Gravity
+static float G = 0.3;
+// Jump speed
+static const float jump = 12;
+static const float move_speed = 5;
+
+struct Player {
+    unsigned int height, width;
+    float x, y;
+    float dx, dy;
+
+    Player(sf::Vector2u size)
+     : Player(size, 0, 0)
+    {
+    }
+
+    Player(sf::Vector2u size, int x, int y)
+      : x(x), y(y)
+    {
+        height = size.y;
+        width = size.x;
+        dx = 0;
+        dy = 0;
+    }
+};
+
+static bool handle_close_events(sf::RenderWindow& win);
+
+int main(void)
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+    int vw, vh;
+    sf::Sprite background;
+    sf::Texture backgroundtex;
+
+    sf::Sprite player;
+    sf::Texture playertex;
+
+    // background stuff
+    backgroundtex.loadFromFile("assets/bck@2x.png");
+    background.setTexture(backgroundtex);
+    vw = backgroundtex.getSize().x;
+    vh = backgroundtex.getSize().y;
+
+    // platform stuff
+    static const int nplatforms = 8;
+    sf::RectangleShape platforms[nplatforms];
+    PlatformCtrl platform_ctrl(platforms, nplatforms);
+    platform_ctrl.set_bounds(vw, vh);
+    platform_ctrl.randomize();
+
+    // player stuff
+    playertex.loadFromFile("assets/PixelThing.png");
+    player.setTexture(playertex);
+    sf::Vector2u pSize = playertex.getSize();
+    Player p( pSize, vw / 2, vh );
+
+    // Create the window
+    sf::VideoMode video(vw, vh);
+    sf::RenderWindow win(video, "Not Doodle Jump ;)", sf::Style::Close);
+    win.setFramerateLimit(60);
+    while (win.isOpen())
+    {
+        // Handle window events
+        if (handle_close_events(win))
+            return 0;
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+        {
+            player.setScale(sf::Vector2f(-1, 1));
+            p.dx = -move_speed; // Moves Left
+        }
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+        {
+            player.setScale(sf::Vector2f(1, 1));
+            p.dx =  move_speed; // Moves Right
+        }
+
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+            if(p.dy == 0)
+                p.dy = -jump; // Jumps Up if not in air already
+
+        if(!(platform_ctrl.hit_platform(p.x + p.width , p.y + p.height) || platform_ctrl.hit_platform(p.x , p.y + p.height)))
+        {
+            p.dy += G;
+        }
+
+        p.dx *= 0.85; // friction/wind-resistance... something like that
+        p.x  += p.dx;
+        p.y  += p.dy;
+
+        // Bottom edge detection
+        if (p.y > vh-p.height)
+        {
+            // TODO this is where the player looses
+            p.y = vh-p.height;
+            p.dy = 0;
+        }
+
+        if (p.x > vw + 32)
+        {
+            p.x = -32;
+        }
+        else if (p.x < -32)
+        {
+            p.x = vw + 32;
+        }
+
+        if(platform_ctrl.hit_platform(p.x + p.width , p.y + p.height) || platform_ctrl.hit_platform(p.x , p.y + p.height))
+        {
+            p.dy = 0;
+        }
+
+        player.setPosition(p.x, p.y);
+
+        win.draw(background);
+        for (int i = 0; i < nplatforms; i++)
+            win.draw(platforms[i]);
+        win.draw(player);
+        win.display();
+    }
+    return 0;
+}
+
+static bool handle_close_events(sf::RenderWindow& win)
+{
+    sf::Event event;
+    while (win.pollEvent(event))
+        switch (event.type)
+        {
+        case sf::Event::Closed:
+            // Handle the "x" button
+            win.close();
+            return true;
+        case sf::Event::KeyPressed:
+            switch (event.key.code)
+            {
+            case sf::Keyboard::Q:
+            case sf::Keyboard::Key::Escape:
+                // Close on "Esc"
+                win.close();
+                return true;
+            }
+            break;
+        }
+    return false;
 }
